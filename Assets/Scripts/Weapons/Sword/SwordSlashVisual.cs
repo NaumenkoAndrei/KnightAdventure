0a3242b7836c5d37d5a4b5c86e86e596627e0cba
NaumenkoAndrei <98833014+NaumenkoAndrei@users.noreply.github.com>
2024-03-16 01:02:28 +0400

    Скрипт визуализации слеша меча

---
+++ Assets/Scripts/Weapons/Sword/SwordSlashVisual.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class SwordSlashVisual : MonoBehaviour {
+    [SerializeField] private Sword _sword;
+
+    private const string ATTACK = "Attack";
+    private Animator _animator;
+
+    private void Awake() {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Start() {
+        _sword.OnSwordSwing += Sword_OnSwordSwing;
+    }
+
+    private void Sword_OnSwordSwing(object sender, EventArgs e) {
+        _animator.SetTrigger(ATTACK);
+    }
+}
