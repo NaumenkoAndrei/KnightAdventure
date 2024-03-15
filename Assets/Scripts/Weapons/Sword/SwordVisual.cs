7f6ca9305679b47067884279b97effbedca4e30e
NaumenkoAndrei <98833014+NaumenkoAndrei@users.noreply.github.com>
2024-03-16 01:02:19 +0400

    Скрипт визуализации меча

---
+++ Assets/Scripts/Weapons/Sword/SwordVisual.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class SwordVisual : MonoBehaviour {
+    [SerializeField] private Sword _sword;
+
+    private Animator _animator;
+    private const string ATTACK = "Attack";
+
+    private void Awake() {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Start() {
+        _sword.OnSwordSwing += Sword_OnSwordSwing;
+    }
+
+    private void Sword_OnSwordSwing(object sender, EventArgs e) {
+        _animator.SetTrigger(ATTACK);
+    }
+
+    public void TriggerEndAttackAnimation() {
+        _sword.AttackColliderTurnOff();
+    }
+}
