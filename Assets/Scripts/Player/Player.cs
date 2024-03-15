253e8920e4a1b4f5c62d0d4480537c1839aca68a
NaumenkoAndrei <98833014+NaumenkoAndrei@users.noreply.github.com>
2024-03-16 00:59:35 +0400

    Скрипт занимающийся логической частью игрока

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+
+[SelectionBase]
+public class Player : MonoBehaviour {
+    public static Player Instance { get; private set; }
+    public bool IsRunning { get; private set; }
+
+    [SerializeField] private float _movingSpeed = 5f;
+
+    private Rigidbody2D _rb;
+    private float _minMovingSpeed = 0.1f;
+    private Vector2 _movementVector;
+
+    private void Awake() {
+        Instance = this;
+        IsRunning = false;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start() {
+        GameInput.Instance.OnPlayerAttack += GameInput_OnPlayerAttack;
+    }
+
+    private void GameInput_OnPlayerAttack(object sender, EventArgs e) {
+        ActiveWeapon.Instance.GetActiveWeapon().Attack();
+    }
+
+    private void Update() {
+        _movementVector = GameInput.Instance.GetMovementVector();
+    }
+
+    private void FixedUpdate() {
+        HandleMovement();
+    }
+
+    private void HandleMovement() {
+        _rb.MovePosition(_rb.position + _movementVector * (_movingSpeed * Time.fixedDeltaTime));
+
+        if (Mathf.Abs(_movementVector.x) > _minMovingSpeed || Mathf.Abs(_movementVector.y) > _minMovingSpeed) {
+            IsRunning = true;
+        } else {
+            IsRunning = false;
+        }
+    }
+
+    public Vector2 GetPlayerScreenPosition() {
+        Vector2 playerScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        return playerScreenPosition;
+    }
+}
